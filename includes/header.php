ae0416ce36e0a28024a0467fe49eea994331c05a
Marcelo Schmitt <marcelo.rauh@gmail.com>
2025-12-30 16:29:30 -0300

    Ajuste para celular

--- includes/header.php
+++ includes/header.php
@@ -14,7 +14,10 @@ if (!$user->isLoggedIn() && !in_array(basename($_SERVER['PHP_SELF']), $allowed_p
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
     <title><?php echo isset($page_title) ? $page_title : 'Sistema de Controle - IFRS'; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css" rel="stylesheet">
