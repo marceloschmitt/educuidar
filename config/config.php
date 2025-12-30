832899f7a799c8a6df94ff6ce2af1ac5899f013b
Marcelo Schmitt <marcelo.rauh@gmail.com>
2025-12-30 11:16:14 -0300

    Add config/config.php to repository

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Configuration file
+ * Fill in your database credentials after installation
+ */
+
+return [
+    'database' => [
+        'host' => 'localhost',
+        'db_name' => 'educuidar',
+        'username' => 'root',
+        'password' => 'root'
+    ]
+];
+
